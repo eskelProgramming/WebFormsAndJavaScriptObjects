a4c0432055736a0c31e74d2b03ac510af30cbc6f
eskelprogramming <eskelley0673@students.cptc.edu>
2024-04-30 12:42:18 -0800

    Refactor form processing logic and add input validation in main.js and main.ts

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 class Plant {
 }
+window.onload = function () {
+    let addPlantBtn = document.querySelector("#add-plant");
+    addPlantBtn.onclick = processPlant;
+};
+function processPlant() {
+    let userPlant = getPlant();
+    if (userPlant != null) {
+        addPlant(userPlant);
+    }
+}
+function getPlant() {
+    return null;
+}
+function isValidInput(id, errorMsg) {
+    return "";
+}
+function addPlant(plant) {
+    alert("Adding plant data was valid, plant added");
+    console.log(plant);
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,60 @@
+/**
+ * Represents a single plant that the user wants to keep track of.
+ */
 class Plant {
     name : string;
     type : string;
     wateringFrequency : string;
     sunlight : string;
+}
+
+/**
+ * When the form is submitted, the processPlant function is called.
+ */
+window.onload = function() {
+    // Set up the button click for add plant form
+    let addPlantBtn = document.querySelector("#add-plant") as HTMLButtonElement;
+    addPlantBtn.onclick = processPlant;
+}
+
+function processPlant() {
+    let userPlant = getPlant();
+    if (userPlant != null) {
+        addPlant(userPlant);
+    }
+}
+
+/**
+ * Check if the input on the form is valid and
+ * returns a Plant object if valid or null if invalid/empty.
+ * @returns Plant object or null if invalid input
+ */
+function getPlant():Plant {
+    
+
+
+    return null;
+}
+
+// 
+function isValidInput(id:string, errorMsg:string):string {
+    // Get the values from the form
+
+    // Validate plant name
+    
+    // Validate plant type
+
+    // Validate watering frequency
+
+    // Validate sunlight
+
+    return "";
+}
+
+/**
+ * Displays the plant data in the console and alerts the user.
+ */
+function addPlant(plant:Plant):void {
+    alert("Adding plant data was valid, plant added");
+    console.log(plant);
 }
